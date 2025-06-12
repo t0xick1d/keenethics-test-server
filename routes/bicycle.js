7b36f6806a74e25e3616cadd1da4bc64a126b904
t0xick1d <flaider1999@gmail.com>
2025-06-12 13:59:13 +0300

    remove database

--- routes/bicycle.js
+++ routes/bicycle.js
@@ -8,18 +8,18 @@ const { validateBody, isValidId } = require('../middleweres');
 
 const { schema, updateSchema } = require('../models/bicycle');
 
-router.get('/', ctrl.getAll);
+// router.get('/', ctrl.getAll);
 
 router.get('/search', ctrl.search);
 
 router.get('/tickets/:searchId', ctrl.getTickets);
 
-router.get('/:bicycleId', isValidId, ctrl.getById);
+// router.get('/:bicycleId', isValidId, ctrl.getById);
 
-router.post('/', validateBody(schema), ctrl.add);
+// router.post('/', validateBody(schema), ctrl.add);
 
-router.delete('/:bicycleId', isValidId, ctrl.deleteById);
+// router.delete('/:bicycleId', isValidId, ctrl.deleteById);
 
-router.patch('/:bicycleId/update', isValidId, validateBody(updateSchema), ctrl.updateById);
+// router.patch('/:bicycleId/update', isValidId, validateBody(updateSchema), ctrl.updateById);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const mongoose = require('mongoose');
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
-mongoose.set('strictQuery', true);
+app.listen(PORT);
 
-mongoose
-   .connect(DB_HOST)
-   .then(() => {
-      app.listen(PORT);
-      console.log('Database connection successful');
-   })
-   .catch((error) => {
-      console.log(error.message);
-      process.exit(1);
-   });
+// mongoose.set('strictQuery', true);
+
+// mongoose
+//    .connect(DB_HOST)
+//    .then(() => {
+//       console.log('Database connection successful');
+//    })
+//    .catch((error) => {
+//       console.log(error.message);
+//       process.exit(1);
+//    });
